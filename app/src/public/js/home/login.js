0914061eb969009cdf9bce5cbfa33b544c6df588
sanghee01 <sang6299@naver.com>
2022-07-15 09:49:21 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -8,7 +8,15 @@ loginBtn.addEventListener("click", login);
 
 function login() {
   const req = {
-    id: id.value,
+    id: id.value, // value로 해당 값 가져올 수 있음
     psword: psword.value,
   };
+
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
 }
